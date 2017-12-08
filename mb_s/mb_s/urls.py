365f925dcfbbf46b6f1613588acdffad76bfa7d6
Mark Chang <ak0922747401@gmail.com>
2017-12-08 22:03:19 +0800

    add image management moudle and init db moudle

--- mb_s/mb_s/urls.py
+++ mb_s/mb_s/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('image_prediction.urls')),
+    url(r'^', include('image_management.urls')),
 ]

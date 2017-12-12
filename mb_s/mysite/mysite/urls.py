357c9c3b097a30287dc44e3cb90b576d1431e846
mark-PC\mark <ak0922747401@gmail.com>
2017-12-12 16:59:03 +0800

    clear useless moudle

--- mb_s/mysite/mysite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from . import views
-
-urlpatterns = [
-    #url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index'),
-]

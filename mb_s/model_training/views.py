dcdad029517be23e21e53745ed0bf24d40ab9c8b
mark-PC\mark <ak0922747401@gmail.com>
2017-12-18 18:05:29 +0800

    add fool-proofing to some functions

--- mb_s/model_training/views.py
+++ mb_s/model_training/views.py
@@ -11,7 +11,10 @@ graph = tf.get_default_graph()
 class ModelTrainingView(View):
     def post(self, request):
         challenge_id = request.POST['challenge_id']
-        with graph.as_default():
-            ModelTrainingServices.run(challenge_id)
-        K.clear_session()
-        return HttpResponseRedirect(reverse('image_class_management:show_image_class', args=[challenge_id]))
+        try:
+            with graph.as_default():
+                ModelTrainingServices.run(challenge_id)
+            K.clear_session()
+            return HttpResponseRedirect(reverse('image_class_management:show_image_class', args=[challenge_id]))
+        except Exception as e:
+            return HttpResponseRedirect(reverse('image_class_management:show_image_class', args=[challenge_id]))

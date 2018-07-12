37351117d2fe0fe1ab711d0d5a07b0652b60757b
Mark Chang <ak0922747401@gmail.com>
2018-07-12 22:14:38 +0800

    refactor image prediction and add unit test

--- mb_s/image_prediction/views.py
+++ mb_s/image_prediction/views.py
@@ -6,17 +6,18 @@ from image_prediction.services import ImagePredictServices
 from keras import  backend as K
 
 graph = tf.get_default_graph()
-logging.basicConfig(level=logging.DEBUG)
-log = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 class ImagePredictView(View):
     def get(self, request):
-        context = ImagePredictServices.get_prediction_context()
+        service = ImagePredictServices()
+        context = service.get_prediction_context()
         return render(request, 'image_prediction/prediction.html', context)
     
     def post(self, request):
         with graph.as_default():
-            result = ImagePredictServices.run(request.POST['model_id'])
+            service = ImagePredictServices()
+            result = service.run(request.POST['model_id'])
         K.clear_session()
-        context = ImagePredictServices.get_prediction_context(result)
+        context = service.get_prediction_context(result)
         return render(request, 'image_prediction/prediction.html', context)
